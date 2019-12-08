c300401b5ef484795ebebc9f6fcccc826244873d
Hylke Visser <git@htdvisser.nl>
2019-12-08 17:49:48 +0100

    backbone: Add UDP packet server

---
+++ backbone/server/packet/server.go
@@ -0,0 +1,94 @@
+// Package packet provides the backbone for a UDP packet server with some opinionated defaults.
+package packet
+
+import (
+	"context"
+	"net"
+	"sync"
+)
+
+// Handler is the interface for handling packets.
+type Handler interface {
+	HandlePacket(context.Context, []byte, net.Addr, func([]byte) error) error
+}
+
+// HandlerFunc is the Handler func.
+type HandlerFunc func(context.Context, []byte, net.Addr, func([]byte) error) error
+
+// HandlePacket implements the Handler interface.
+func (f HandlerFunc) HandlePacket(ctx context.Context, pkt []byte, addr net.Addr, reply func([]byte) error) error {
+	return f(ctx, pkt, addr, reply)
+}
+
+// Server implements a UDP packet server.
+type Server struct {
+	baseContext                 context.Context
+	extendContextWithPacketConn func(context.Context, net.PacketConn) context.Context
+	extendContextWithRemoteAddr func(context.Context, net.Addr) context.Context
+
+	middleware []Middleware
+	handler    Handler
+
+	mu       sync.Mutex
+	bindings map[net.PacketConn]struct{}
+}
+
+// NewServer instantiates a new packet server with the given options.
+func NewServer(handler Handler, opts ...Option) *Server {
+	options := &options{}
+	options.apply(opts...)
+	return &Server{
+		middleware: options.middleware,
+		handler:    handler,
+		bindings:   make(map[net.PacketConn]struct{}),
+	}
+}
+
+func (s *Server) Serve(conn net.PacketConn) error {
+	s.mu.Lock()
+	s.bindings[conn] = struct{}{}
+	s.mu.Unlock()
+	connCtx := context.Background()
+	if s.baseContext != nil {
+		connCtx = s.baseContext
+	}
+	if s.extendContextWithPacketConn != nil {
+		connCtx = s.extendContextWithPacketConn(connCtx, conn)
+		if connCtx == nil {
+			panic("extendContextWithPacketConn returned a nil context")
+		}
+	}
+	handler := chain(s.handler, s.middleware...)
+	var buf [0xffff]byte
+	for {
+		n, addr, err := conn.ReadFrom(buf[:])
+		if n > 0 {
+			pkt := make([]byte, n)
+			copy(pkt, buf[:n])
+			pktCtx := connCtx
+			if s.extendContextWithRemoteAddr != nil {
+				pktCtx = s.extendContextWithRemoteAddr(pktCtx, addr)
+				if pktCtx == nil {
+					panic("extendContextWithRemoteAddr returned a nil context")
+				}
+			}
+			handler.HandlePacket(pktCtx, pkt, addr, func(res []byte) error {
+				_, err := conn.WriteTo(res, addr)
+				return err
+			})
+		}
+		if err != nil {
+			return err
+		}
+	}
+}
+
+// GracefulStop stops the stream server gracefully.
+func (s *Server) GracefulStop() error {
+	s.mu.Lock()
+	for conn := range s.bindings {
+		conn.Close()
+	}
+	s.mu.Unlock()
+	return nil
+}
