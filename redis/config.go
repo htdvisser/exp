ece871a1b1255097360e42bd9c5c91d6240c4b97
Hylke Visser <git@htdvisser.nl>
2020-07-29 21:35:26 +0200

    redis: Support loading password from file

--- redis/config.go
+++ redis/config.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"crypto/tls"
+	"io/ioutil"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/spf13/pflag"
@@ -10,11 +11,12 @@ import (
 
 // Config is the configuration for connecting to Redis.
 type Config struct {
-	Addresses []string
-	Username  string
-	Password  string
-	PoolSize  int
-	TLSConfig *tls.Config
+	Addresses    []string
+	Username     string
+	Password     string
+	PasswordFile string
+	PoolSize     int
+	TLSConfig    *tls.Config
 }
 
 // Flags returns a flagset that can be added to the command line.
@@ -23,6 +25,7 @@ func (c *Config) Flags(prefix string) *pflag.FlagSet {
 	flags.StringSliceVar(&c.Addresses, prefix+"addresses", []string{"localhost:6379"}, "Redis addresses")
 	flags.StringVar(&c.Username, prefix+"auth.username", "", "Redis username")
 	flags.StringVar(&c.Password, prefix+"auth.password", "", "Redis password")
+	flags.StringVar(&c.PasswordFile, prefix+"auth.password-file", "", "Redis password file")
 	flags.IntVar(&c.PoolSize, prefix+"pool.size", 0, "Redis connection pool size")
 	return &flags
 }
@@ -35,6 +38,13 @@ func (c Config) Connect(ctx context.Context) (redis.UniversalClient, error) {
 		Password:  c.Password,
 		PoolSize:  c.PoolSize,
 		TLSConfig: c.TLSConfig,
+	}
+	if c.PasswordFile != "" {
+		passwordBytes, err := ioutil.ReadFile(c.PasswordFile)
+		if err != nil {
+			return nil, err
+		}
+		opts.Password = string(passwordBytes)
 	}
 	cli := redis.NewUniversalClient(&opts)
 	if err := cli.Ping(ctx).Err(); err != nil {
