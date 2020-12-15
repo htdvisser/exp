6786571a568ef3a2c67093c7493d80641043f863
Hylke Visser <hi@htdvisser.dev>
2020-12-15 12:07:38 +0100

    rjs: Replace SetupRequestItem with explicit fields in SetupMixedRequest

--- rjs/router/setup.go
+++ rjs/router/setup.go
@@ -29,9 +29,18 @@ type SetupBulkRequest struct {
 }
 
 type SetupMixedRequest struct {
-	OwnerID types.ID6 `json:"ownerid"`
-	SetupRequestItem
-	Routers []SetupRequestItem `json:"routers"`
+	OwnerID   types.ID6          `json:"ownerid"`
+	CUPSURI   string             `json:"cupsUri,omitempty"`
+	CUPSKey   []byte             `json:"cupsKey,omitempty"`
+	CUPSCrt   []byte             `json:"cupsCrt,omitempty"`
+	CUPSTrust []byte             `json:"cupsTrust,omitempty"`
+	LNSURI    string             `json:"lnsUri,omitempty"`
+	LNSKey    []byte             `json:"lnsKey,omitempty"`
+	LNSCrt    []byte             `json:"lnsCrt,omitempty"`
+	LNSTrust  []byte             `json:"lnsTrust,omitempty"`
+	FWCRC     int32              `json:"fwcrc,omitempty"`
+	FWAfter   *types.Time        `json:"fwafter,omitempty"`
+	Routers   []SetupRequestItem `json:"routers"`
 }
 
 type SetupResponseItem struct {
